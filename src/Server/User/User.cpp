c2a31a416d5027737619ccc926c451a9f00a1e31
otero-true <xlopec0mq0@gmail.com>
2024-04-11 21:50:53 +0300

    Added location user for class user.

--- src/Server/User/User.cpp
+++ src/Server/User/User.cpp
@@ -2,10 +2,21 @@
 
 User::User(std::string username, std::string address)
 {
+	this->userLocation = MENU;
 	this->username = username;
 	this->address = address;
 }
 
 User::~User()
 {
+}
+
+void User::setLocation(Location l)
+{
+	this->userLocation = l;
+}
+
+Location User::getLocation()
+{
+	return this->userLocation;
 }
--- src/Server/User/User.h
+++ src/Server/User/User.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <string>
 
+enum Location {
+	MENU,
+	LOBBY,
+	GAME
+};
+
 class User {
 public:
 	User() = delete;
@@ -9,7 +15,10 @@ public:
 	bool operator==(const User& other) {
 		return this->username == other.username && this->address == other.address;
 	}
+	void setLocation(Location l);
+	Location getLocation();
 private:
+	Location userLocation;
 	std::string username;
 	std::string address;
 };
