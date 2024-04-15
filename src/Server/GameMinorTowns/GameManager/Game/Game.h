b185f17cd9d0629cfa2f957f20254d9d5e10b58f
otero-true <xlopec0mq0@gmail.com>
2024-04-15 10:48:09 +0300

    fix last commit

--- src/Server/GameMinorTowns/GameManager/Game/Game.h
+++ src/Server/GameMinorTowns/GameManager/Game/Game.h
@@ -3,12 +3,14 @@
 #include <vector>
 #include <string>
 
+class User;
+
 class Game {
 public:
 	Game() = delete;
 	Game(std::vector<std::shared_ptr<User>> users);
 	~Game() {}
-	const std::string getUUIDgit();
+	const std::string getUUID();
 private:
 	std::vector<std::shared_ptr<User>> users;
 	std::string uuid;
